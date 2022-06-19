b6483a00e996722fa48b946434b2ef23f935949e
Denis Reshetnyak <denresh81@yandex.ru>
2022-06-20 00:07:44 +0700

    backend4

--- app.js
+++ app.js
@@ -20,5 +20,8 @@ app.use((req, res, next) => {
 });
 app.use('/users', users);
 app.use('/cards', cards);
+app.use((req, res) => {
+  res.status(404).send({ message: 'Страницы не существует' });
+});
 
 app.listen(PORT);
