601dce4478ed277bf21564548188823b89d8bd3b
Denis Reshetnyak <denresh81@yandex.ru>
2022-07-04 18:37:48 +0700

    mesto14

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.post('/signin', celebrate({
   }),
 }), login);
 
-app.post('/signup', celebrate({
+/*app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
@@ -40,6 +40,6 @@ app.post('/signup', celebrate({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
-}), createUser);
+}), createUser);*/
 
 app.listen(PORT);
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const {
 } = require('../controllers/users');
 
 router.get('/', getUsers);
+
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
     userId: Joi.string().length(24).hex().required(),
